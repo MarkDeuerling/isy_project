cece0e407b5b4f9533449050d04d9f1633eda454
s64630 <s64630@beuth-hochschule.de>
2018-07-08 12:35:01 +0200

    testing around

--- gesture_detector.py
+++ gesture_detector.py
@@ -14,13 +14,14 @@ loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
 model = load_model("./cnn_trainer/model.h5")
 
+
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # prediction_array = loaded_model.predict_classes(frame)
-    # prediction_array = model.predict(frame, verbose=1)
-    # print(prediction_array)
+    prediction_array = model.predict_classes(frame)
+    prediction_array = model.predict(frame, verbose=1)
+    print(prediction_array)
 
     cv2.imshow('video feed', frame)  # show video stream#
     keyInput = cv2.waitKey(1)  # wait one millisecond for key input by user
